be54d8bb5a9ec2cef76fdffb31938f6d314c7054
Priyal Vyas <priyal84vyas@gmail.com>
2021-10-18 23:44:09 +0530

    navigation to top added

--- js/backtotop.js
+++ js/backtotop.js
@@ -14,11 +14,10 @@ window.onscroll = function () {
   scrollFunction();
 };
 
-
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
-
 function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
