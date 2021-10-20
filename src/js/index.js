cd5a3e96ef0356b6bacb6d0383f8a2d43ab74ccb
OneLiL05 <kirillofficial0507@gmail.com>
2021-10-20 21:48:30 +0300

    conflicts hot-fix

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,9 @@
 const counterElements = document.getElementsByClassName("count");
 
 function updateCounter() {
-	let counterElement, rest;
+	const counterElement, rest;
 	[counterElement, ...rest] = counterElements;
-	
+
 	fetch("https://api.countapi.xyz/update/uimonk/youtubechannel/?amount=1")
 		.then((res) => res.json())
 		.then((data) => (counterElement.innerHTML = data.value));
