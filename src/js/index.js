20e1677ed209abf22e6f5c9141ef99d4219ca653
OneLiL05 <kirillofficial0507@gmail.com>
2021-10-20 20:58:33 +0300

    addded linters

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 function updateCounter() {
-  fetch('https://api.countapi.xyz/update/uimonk/youtubechannel/?amount=1')
- .then(res => res.json())
- .then(data => counterElement.innerHTML = data.value)
- }
- updateCounter()
- counterElement = document.getElementsByClassName('count')[0];
+	fetch("https://api.countapi.xyz/update/uimonk/youtubechannel/?amount=1")
+		.then((res) => res.json())
+		.then((data) => (counterElement.innerHTML = data.value));
+}
+updateCounter();
+counterElement = document.getElementsByClassName("count")[0];
