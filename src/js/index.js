5879d1ef5dfad19efc17e289e1f383518798c8e3
OneLiL05 <kirillofficial0507@gmail.com>
2021-10-20 21:14:42 +0300

    fix conflict

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
+const counterElement = document.getElementsByClassName("count")[0];
+
 function updateCounter() {
 	fetch("https://api.countapi.xyz/update/uimonk/youtubechannel/?amount=1")
 		.then((res) => res.json())
 		.then((data) => (counterElement.innerHTML = data.value));
 }
+
 updateCounter();
-counterElement = document.getElementsByClassName("count")[0];
